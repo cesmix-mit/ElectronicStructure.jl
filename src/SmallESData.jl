027f27916121d6d9ee96b4804a4a8bf0d394e6cb
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2021-12-01 21:29:54 -0500

    adding linearize function

--- src/SmallESData.jl
+++ src/SmallESData.jl
@@ -1,9 +1,9 @@
-export SmallESData, gen_test_data
+export SmallESData, gen_test_data, linearize
 
 struct SmallESData{D} <: ElectronicStructureData{D}
     energies::Vector{Unitful.Energy}
     forces::Vector{Vector{SVector{D, Unitful.Force}}}
-    stresses::Vector{SMatrix{D, D, Float64}} # Q: stress type in Unitful?
+    stresses::Vector{SMatrix{D, D, Unitful.Pressure}} # TODO: stress type in Unitful?
 end
 
 """
@@ -17,5 +17,14 @@ function gen_test_data(D, atomic_confs, p)
     s   = [ virial_stress(s, p) for s in atomic_confs ]
     data = SmallESData{D}(pe, f, s)
     return data
+end
+
+"""
+    linearize(v::Vector)
+    
+Linearize a vector of forces, or a vector of stresses. Units are removed.
+"""
+function linearize(v::Vector)
+    return map(x->x.val, reduce(vcat, map(vec, v)))
 end
 
