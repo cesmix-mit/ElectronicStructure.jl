f15527e94eb75a934c406f8879ce3453e20ba1a0
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2021-12-02 15:51:46 -0500

    advances in data linearization/flattening

--- src/SmallESData.jl
+++ src/SmallESData.jl
@@ -19,12 +19,35 @@ function gen_test_data(D, atomic_confs, p)
     return data
 end
 
+
+"""
+    lower_tri_vec(s::SMatrix{D, D}) where {D}
+    
+ Returns a vector containing the lower triangle of a matrix.
+"""
+function lower_tri_vec(s::SMatrix{D, D}) where {D}
+    return [ s[i, j]  for j in 1:3 for i in j:3 ]
+end
+
 """
     linearize(v::Vector)
     
 Linearize a vector of forces, or a vector of stresses. Units are removed.
 """
 function linearize(v::Vector)
-    return map(x->x.val, reduce(vcat, map(vec, v)))
+    return map(x->x.val, vcat(map(vec, v)...))
 end
+
+"""
+    linearize(d::SmallESData)
+    
+Linearize the energies, forces, and stresses of a SmallESData struct. Units are removed.
+"""
+function linearize(d::SmallESData)
+    return vcat( map(x->x.val, d.energies), 
+                 vcat(linearize.(d.forces)...),
+                 linearize(lower_tri_vec.(d.stresses)))
+end
+
+
 
